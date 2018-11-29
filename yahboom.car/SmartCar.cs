312c907d19308a15bad91ac849f3d1cd4c5df6d3
liuqing <285191429@qq.com>
2018-11-29 17:51:20 +0800

    car v1

---
+++ yahboom.car/SmartCar.cs
@@ -0,0 +1,37 @@
+﻿using Com.Enterprisecoding.RPI.GPIO;
+using Com.Enterprisecoding.RPI.GPIO.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yahboom.car
+{
+    public class SmartCar
+    {
+
+        //定义引脚
+        const int Left_motor_go = 28;       //左电机前进AIN2连接Raspberry的wiringPi编码28口
+        const int Left_motor_back = 29;     //左电机后退AIN1连接Raspberry的wiringPi编码29口
+
+        const int Right_motor_go = 24;      //右电机前进BIN2连接Raspberry的wiringPi编码24口
+        const int Right_motor_back = 25;    //右电机后退BIN1连接Raspberry的wiringPi编码25口
+
+        const int Left_motor_pwm = 27;      //左电机控速PWMA连接Raspberry的wiringPi编码27口
+        const int Right_motor_pwm = 23;     //右电机控速PWMB连接Raspberry的wiringPi编码23口
+
+        public void run(int LeftCarSpeedControl, int RightCarSpeedControl) {
+
+
+            WiringPi.Core.DigitalWrite(Left_motor_go, DigitalValue.High); //左电机前进使能
+            WiringPi.Core.DigitalWrite(Left_motor_back, DigitalValue.Low); //左电机后退禁止
+            WiringPi.Core.PWMWrite(Left_motor_pwm, LeftCarSpeedControl); // TODO: softPwmWrite
+
+            WiringPi.Core.DigitalWrite(Right_motor_go, DigitalValue.High); //右电机前进使能
+            WiringPi.Core.DigitalWrite(Right_motor_back, DigitalValue.Low); //右电机后退禁止
+            WiringPi.Core.PWMWrite(Right_motor_pwm, LeftCarSpeedControl);
+
+
+        }
+    }
+}
---
+++ yahboom.car/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace yahboom.car
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+
+
+            app.Map("/ws", SocketHandler.Map);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
